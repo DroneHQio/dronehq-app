831aeafc3897a3295dbe45ecb1661b53d0d182e2
DroneHQio <mtnr.fb@gmail.com>
2025-08-05 14:20:55 -0500

    Update student.js

--- pages/signup/student.js
+++ pages/signup/student.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { supabase, signUp } from '../lib/supabase'
+import { supabase, signUp } from '../../lib/supabase'
 import { useRouter } from 'next/router'
 
 export default function StudentSignup() {
